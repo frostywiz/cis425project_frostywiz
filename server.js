cdfed0ba93f0f9d0656d5ab6985c41a4b3e642e5
Caleb Frost <148497803+frostywiz@users.noreply.github.com>
2023-11-28 15:12:51 -0700

    Add files via upload
    Added server.js that is based off of the correct references to the contact.html file.

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+    host: 'sql.wpc-is.online',
+    user: 'ctfrost',
+    password: 'ctfr0667',
+    database: 'db_test_ctfrost',
+});
+
+con.connect();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    const indexPath = path.join(__dirname, 'index.html');
+    res.sendFile(indexPath);
+});
+
+// This gets request from the contact page after hitting submit.
+app.post('/customer', (req, res) => {
+    const { firstname, lastname, email, phone, address, reason, message } = req.body;
+
+    console.log('Request body: ', req.body);
+
+    con.query(
+        'INSERT INTO contact (firstname, lastname, email, phone, address, reason, message) VALUES (?, ?, ?, ?, ?, ?, ?)',
+        [firstname, lastname, email, phone, address, reason, message],
+        (error, results) => {
+            if (error) {
+                console.error('Error inserting data into the database: ', error);
+                res.status(500).json({ error: 'Database error', details: error.message });
+            } else {
+                console.log('Data inserted successfully');
+                res.status(200).json({ success: true });
+            }
+        }
+    );
+});
+
+const port = 8200;
+app.listen(port, () => {
+    console.log(`The web server is alive! \nListening on http://localhost:${port}`);
+});
